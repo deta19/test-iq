7bb14b6ab8cf01bd2c7452369aa4cebbafdee628
deta19 <deta.mihai@gamil.com>
2025-05-12 22:32:35 +0300

    fix Task #4

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,13 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, MustVerifyEmail, HasRoles;
-
+    
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
